198953075b703ade9ffa20d794871203e30199c6
Dooky <chamallow14@hotmail.fr>
2025-11-12 22:37:59 -0300

    typescript assertions

---
+++ client/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// TypeScript requires to assert the type of the returned element
+// creates a root container that React uses to manage rendering efficiently
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
