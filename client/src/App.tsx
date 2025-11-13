76ed6fac4e63402671c8d626919ec900fd8be8e1
Dooky <chamallow14@hotmail.fr>
2025-11-13 00:24:04 -0300

    skeleton App

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+/* STYLING */
+import './App.css';
+/* REACT */
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/* COMPONENTS */
+import Header from './components/Header';
+import Footer from './components/Footer';
+/* PAGES */
+import HomePage from './pages/HomePage';
+import MyFossilsPage from './pages/MyFossilsPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/myfossils" element={<MyFossilsPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
