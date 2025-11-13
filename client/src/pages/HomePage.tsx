cd84ebe0314ac39a203924572513328efb3da424
Dooky <chamallow14@hotmail.fr>
2025-11-13 14:02:48 -0300

    HomePage v1

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "../components/HeroSection";
+import MapSection from "../components/MapSection";
+
+function HomePage() {
+    return (
+        <>
+            <HeroSection />
+            <MapSection />
+        </>
+    );
+}
+
+export default HomePage;
