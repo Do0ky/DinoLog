2b453f49d27a9f7111bdc5083f23dafcda29467b
Dooky <chamallow14@hotmail.fr>
2025-11-14 22:22:14 -0300

    MapSection v0

--- client/src/components/MapSection.tsx
+++ client/src/components/MapSection.tsx
@@ -1,6 +1,6 @@
 function MapSection() {
     return (
-        <section className="map-section">
+        <section id="discoveries" className="map-section">
             <div className="map-placeholder">🗺️ Map will go here</div>
         </section>
   );
