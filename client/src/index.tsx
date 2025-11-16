13354bcfa1295a1b354966ad931a81297bbec5c3
Dooky <chamallow14@hotmail.fr>
2025-11-16 19:09:46 -0300

    leaflet

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'leaflet/dist/leaflet.css';
 
 // TypeScript requires to assert the type of the returned element
 // creates a root container that React uses to manage rendering efficiently
